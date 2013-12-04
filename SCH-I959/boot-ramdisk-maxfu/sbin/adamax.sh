8cf9fbe2b6b8e4372a49321a9bfd5b675e330719
Max Fu <fxy83613@gmail.com>
2013-12-04 09:01:39 +0000

    Added auto/force root script.

--- SCH-I959/boot-ramdisk-maxfu/sbin/adamax.sh
+++ SCH-I959/boot-ramdisk-maxfu/sbin/adamax.sh
@@ -51,7 +51,7 @@ if [ ! -f /system/xbin/busybox ]; then
         fi
     done
 fi
-/sbin/busybox mount -o remount,rw /system
+/sbin/busybox mount -o remount,ro /system
 
 # STweaks support
 chmod -R 755 /res/customconfig/actions
@@ -67,7 +67,35 @@ rm /data/.adamkernel/action.cache
 
 sync
 
-# /system/xbin/daemonsu --auto-daemon &
+# Auto/Force root with koush's superuser
+if [ ! -f /system/etc/.koush ]; then
+    # Wipe other superuser suite
+    /sbin/busybox mount -o remount,rw /system
+    /sbin/busybox mount chattr -i /system/bin/su
+    /sbin/busybox mount chattr -i /system/xbin/su
+    /sbin/busybox mount rm -f /system/bin/su
+    /sbin/busybox mount rm -f /system/xbin/su
+    /sbin/busybox mount rm -f /system/app/Superuser.*
+    /sbin/busybox mount rm -f /system/app/Supersu.*
+    /sbin/busybox mount rm -f /system/app/superuser.*
+    /sbin/busybox mount rm -f /system/app/supersu.*
+    /sbin/busybox mount rm -f /system/app/SuperUser.*
+    /sbin/busybox mount rm -f /system/app/SuperSU.*
+    # Install koush's superuser
+    /sbin/busybox cp /res/superuser/su /system/xbin/
+    /sbin/busybox chown 0:0 /system/xbin/su
+    /sbin/busybox chmod 6755 /system/xbin/su
+    /sbin/busybox ln -s /system/xbin/su /system/bin/su
+    /sbin/busybox cp /res/superuser/Superuser.apk /system/app/
+    /sbin/busybox chmod 644 /system/app/Superuser.apk
+    /sbin/busybox touch /system/etc/.koush
+    /sbin/busybox mount -o remount,ro /system
+fi
+
+# Start superuser daemon
+if [ -f /system/etc/.koush ]; then
+    /system/xbin/su --daemon &
+fi
 
 # init.d support
 if [ -d /system/etc/init.d ]; then
